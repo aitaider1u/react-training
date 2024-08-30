f5868fba3541fb7324ea8834e0abfa5e47bb5c01
zinedine <zine.aitaider@yahoo.com>
2024-08-30 10:37:43 +0200

    first app

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,17 @@
+
+function Button(){
+    const styles = {
+        backgroundColor: "rgba(13, 108, 148, 0.954)",
+        color: "azure",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border : "none",
+        cursor: "pointer",
+    }
+
+
+    return (<button  className={styles}>Click me</button>);
+}
+
+export default Button
+  
